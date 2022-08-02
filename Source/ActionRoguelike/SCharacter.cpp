f352f8c5d416e4d402cdfed36dd47e7d0dac12b5
Arkmanpku <98631029+Arkmanpku@users.noreply.github.com>
2022-08-02 21:17:05 +0800

    Added Character Assets & Setup Input
    -Added Character Assets
    -Added MoveForward/Turn Input

---
+++ Source/ActionRoguelike/SCharacter.cpp
@@ -0,0 +1,44 @@
+
+
+#include "SCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+ASCharacter::ASCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
+}
+
+void ASCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void ASCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+
+}
+
---
+++ Source/ActionRoguelike/Soul.cpp
@@ -0,0 +1,21 @@
+
+#include "Soul.h"
+
+ASoul::ASoul()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASoul::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASoul::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
